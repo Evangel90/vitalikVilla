e478714a09c9f6d3783e976688bc2e1cb0ec0007
Evangel90 <oladipoevangel90@gmail.com>
2026-01-31 00:00:41 +0100

    setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="text-center bg-blue-500 p-20">
+          <h1 className='text-2xl px-8'>Vitalik Villa Roaster</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
